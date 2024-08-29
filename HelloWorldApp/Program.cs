9385c672e11e9a816d9ece00ceb44b6a5e54b72e
Míryam <mgomez@codicesoftware.com>
2024-08-29 14:40:14 +0200

    Just minor change in the text

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorldApp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!!");
         }
     }
 }
