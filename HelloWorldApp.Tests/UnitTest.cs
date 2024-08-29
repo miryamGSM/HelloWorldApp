9bb362a997847889a4b2310af10e11608f1e74d5
Míryam <mgomez@codicesoftware.com>
2024-08-29 11:41:03 +0200

    Add test project

---
+++ HelloWorldApp.Tests/UnitTest.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+
+using HelloWorldApp;
+ 
+namespace HelloWorldApp.Tests
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void TestHelloWorldOutput()
+        {
+            using (var sw = new System.IO.StringWriter())
+            {
+                Console.SetOut(sw);
+                Program.Main(new string[] { });
+                
+                var result = sw.ToString().Trim();
+                Assert.Equal("Hello, World!", result);
+            }
+        }
+    }
+}
