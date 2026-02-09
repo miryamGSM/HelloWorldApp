fae622613083e7c2603f74f873fe65803cb530e1
Míryam <miryam.gomez@unity3d.com>
2026-02-09 10:46:13 +0100

    End comment

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 namespace HelloWorldApp
-{////
+{
     public class Program
     {
         public static void Main(string[] args)
@@ -10,3 +10,5 @@ namespace HelloWorldApp
         }
     }
 }
+
+// end comment
